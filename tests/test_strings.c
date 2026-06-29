0beca35e0da7d2587d50461c7f1871687a676221
aymanebenomar <aymanebenomar2005@gmail.com>
2026-06-29 02:44:43 +0100

    test strings also done

---
+++ tests/test_strings.c
@@ -0,0 +1,14 @@
+#include "../includes/piscine.h"
+
+int main()
+{
+	int len;
+
+	len = ft_strlen("aymane");
+	
+	ft_putstr("length of aymane is : ");
+	ft_putchar(len + '0');
+	ft_putchar('\n');
+
+	return 0;
+}
